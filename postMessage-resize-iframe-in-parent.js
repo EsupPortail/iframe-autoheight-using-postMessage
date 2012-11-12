9b77cff88892cb8f19de59f59ba8bf28213137bd
prigaux <prigaux@93fd7c73-ba33-0410-a30b-86d151360d8f>
2012-11-12 10:19:02 +0000

    postMessage-resize-iframe-in-parent.js: instead of ignoring first "resize" event after forced resize, ignore it if it shrinks the window + use shindig dynamic-size-util.js to compute window height
    this fixes the following pb:
    - in case of a page being constructed using javascript,
    - our script is run on "load" event which occurs before the javascript page is finished
    - our script was ignoring first "resize" event, excepting this "resize" to be caused by iframe resize
    - alas, there is a race between the "resize" event triggered by iframe resize and the "resize" event caused by javascript DOM append
    
    the solution is to only ignore "resize" event when the new height is smaller than before.
    
    this commit also switches to shindig getDimen code, since it works quite nicely and removes handcrafted code, relying on shindig's shoulders.
    
    git-svn-id: https://subversion.cru.fr/esup-tools/iframe-autoheight-using-postMessage/trunk@102 93fd7c73-ba33-0410-a30b-86d151360d8f

--- postMessage-resize-iframe-in-parent.js
+++ postMessage-resize-iframe-in-parent.js
@@ -3,63 +3,147 @@ if (!postMessage_resize_iframe_in_parent) {
     postMessage_resize_iframe_in_parent = true;
   (function () {
     var resizedByUs = false;
-    var previousHeights;
+    var previousHeight;
 
     if (parent == window) return;
-
-    var isWebKit = navigator.userAgent.indexOf("WebKit") > -1;
-    var isOpera = navigator.userAgent.indexOf("Opera") > -1;
-    var isIE = (function() {
-        var div = document.createElement('div');
-        div.innerHTML = '<!--[if IE]><i></i><![endif]-->';
-        return (div.getElementsByTagName('i').length === 1);         
-    }());
 
     var target = parent.postMessage ? parent : 
 	(parent.document.postMessage ? parent.document : undefined);
     if (typeof target == "undefined") return;
 
     var mylog = function() {};
-    //if (window['console'] !== undefined) { mylog = function(s) { console.log(s); }; } 
+    if (window['console'] !== undefined) { mylog = function(s) { console.log(s); }; } 
     //else { mylog = function(s) { alert(s); }; }
     
-    var computeHeights = function() {
-	var docElt = document.documentElement
-	return docElt["clientHeight"] + " " + 
-	    docElt["scrollHeight"] + "|" + document.body["scrollHeight"] + " " + 
-	    docElt["offsetHeight"] + "|" + document.body["offsetHeight"];
+
+////////////////////////////////////////////////////////////////////////////////
+// below from shindig dynamic-size-util.js
+////////////////////////////////////////////////////////////////////////////////
+  /**
+   * @private
+   */
+  function getElementComputedStyle(elem, attr) {
+    if (window.getComputedStyle) {
+      var style = window.getComputedStyle(elem, null);
+    } else {
+      var style = elem.currentStyle;
+    }
+    return attr && style ? style[attr] : style;
+  }
+  /**
+   * Parse out the value (specified in px) for a CSS attribute of an element.
+   *
+   * @param {Element} elem the element with the attribute to look for.
+   * @param {string} attr the CSS attribute name of interest.
+   * @return {number} the value of the px attr of the elem, undefined if the attr was undefined.
+   * @private
+   */
+  function parseIntFromElemPxAttribute(elem, attr) {
+    var value = getElementComputedStyle(elem, attr);
+    if (value) {
+      value.match(/^([0-9]+)/);
+      return parseInt(RegExp.$1, 10);
+    }
+  }
+  /**
+   * Get the height (truthy) or width (falsey)
+   */
+  var getDimen = function (height) {
+    var result = 0;
+    var queue = [document.body];
+
+    while (queue.length > 0) {
+      var elem = queue.shift();
+      var children = elem.childNodes;
+
+      /*
+       * Here, we are checking if we are a container that clips its overflow with
+       * a specific height, because if so, we should ignore children
+       */
+
+      // check that elem is actually an element, could be a text node otherwise
+      if (typeof elem.style !== 'undefined' && elem !== document.body) {
+        // Get the overflowY value, looking in the computed style if necessary
+        var overflow = elem.style[height ? 'overflowY' : 'overflowX'];
+        if (!overflow) {
+          overflow = getElementComputedStyle(elem, height ? 'overflowY' : 'overflowX');
+        }
+
+        // The only non-clipping values of overflow is 'visible'. We assume that 'inherit'
+        // is also non-clipping at the moment, but should we check this?
+        if (overflow != 'visible' && overflow != 'inherit') {
+          // Make sure this element explicitly specifies a height
+          var size = elem.style[height ? 'height' : 'width'];
+          if (!size) {
+            size = getElementComputedStyle(elem, height ? 'height' : 'width');
+          }
+          if (size && size.length > 0 && size != 'auto') {
+            // We can safely ignore the children of this element,
+            // so move onto the next in the queue
+            continue;
+          }
+        }
+      }
+
+      for (var i = 0; i < children.length; i++) {
+        var child = children[i];
+        if (typeof child.style != 'undefined') {  // Don't measure text nodes
+          var start = child.offsetTop,
+              dimenEnd = 'marginBottom',
+              size = child.offsetHeight,
+              dir = getElementComputedStyle(child, 'direction');
+
+          if (!height) {
+            start = child.offsetLeft;
+            dimenEnd = 'marginRight';
+            size = child.offsetWidth;
+
+            // compute offsetRight
+            if (dir == 'rtl' && typeof start != 'undefined' && typeof size != 'undefined' && child.offsetParent) {
+              start = child.offsetParent.offsetWidth - start - size;
+            }
+          }
+
+          if (typeof start != 'undefined' && typeof size != 'undefined') {
+            // offsetHeight already accounts for borderBottom, paddingBottom.
+            var end = start + size + (parseIntFromElemPxAttribute(child, dimenEnd) || 0);
+            result = Math.max(result, end);
+          }
+        }
+        queue.push(child);
+      }
     }
 
-    var heightsChanged = function() {
-	var heights = computeHeights();
-	var changed = heights !== previousHeights;
-	previousHeights = heights;
-	if (changed) 
-	    mylog("heights changed: " + heights)
-	//else
-	//    mylog("heights unchanged");
-	return changed;
-    };
+    // Add border, padding and margin of the containing body.
+    return result +
+        (parseIntFromElemPxAttribute(document.body, height ? 'borderBottom' : 'borderRight') || 0) +
+        (parseIntFromElemPxAttribute(document.body, height ? 'marginBottom' : 'marginRight') || 0) +
+        (parseIntFromElemPxAttribute(document.body, height ? 'paddingBottom' : 'paddingRight') || 0);
+  };
+////////////////////////////////////////////////////////////////////////////////
 
-    var computeHeight = function() {
-	var docElt = document.documentElement;
 
-	var height = isWebKit ? docElt["scrollHeight"] : // Chrome & Safari & Opera
-	             isIE ? document.body["scrollHeight"] + 10 :
-	             isOpera ? (docElt["offsetHeight"] > 20 ? docElt["offsetHeight"] : docElt["scrollHeight"]) :
-	             docElt["offsetHeight"]; // Firefox
-	return height;
-    };
     var postMessageIframeHeight = function(kind) {
-        var changed = heightsChanged();
-	if (kind !== "load" && !changed) return;
 
-	var height = computeHeight(kind);
-	if (height > 20) {
+	var height = getDimen(true);
+	var changed = height !== previousHeight;
+	if (changed) 
+	    mylog("heights changed: " + height + " (previousHeight:" + previousHeight + ") ")
+	//else
+	//    mylog("heights unchanged " + heights);
+
+	if (kind !== "load" && !changed) {
+	    // ignore
+	    resizedByUs = false;
+	} else if (kind === "windowResize" && resizedByUs && height < previousHeight) {
+	    mylog("new heights ignored")
+	    resizedByUs = false;
+	} else if (height > 20) {
 	    mylog("sending height " + height);
 	    target.postMessage("iframeHeight " + height, "*");
 	    resizedByUs = true;
         }
+	previousHeight = height;
     };
 
     var intervalId;
@@ -91,15 +175,8 @@ if (!postMessage_resize_iframe_in_parent) {
     };
 
     var windowResize = function(e) {
-        mylog("windowResize");
-	if (resizedByUs) {
-	    mylog("new heights below ignored")
-	    heightsChanged(); // compute new heights which surely must have changed
-	    resizedByUs = false;
-	} else {
-	    postMessageIframeHeight("windowResize");
-	}
-	    
+        mylog("windowResize (resizedByUs:" + resizedByUs + ")");
+	postMessageIframeHeight("windowResize");
     };
 
     var click = function () {
